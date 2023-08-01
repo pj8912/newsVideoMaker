5095138c399b97a80a70477b0e5ec77cda62c829
pj8912 <jpintofun2b@gmail.com>
2023-08-01 22:12:57 +0530

    card news saved to database

--- app.py
+++ app.py
@@ -26,14 +26,21 @@ def save_content():
     news_desc =request.json['news_desc']
     news_image_url =request.json['news_image_url']
     news_source_url =request.json['news_source_url']
-    cursor.execute("INSERT INTO saved_content(n_title,n_desc,n_source_url,n_image_url,) VALUES(?,?,?,?)", (news_title,news_desc,news_image_url,news_source_url))
+    cursor.execute("INSERT INTO saved_content(n_title,n_desc,n_source_url,n_image_url) VALUES(?,?,?,?)",(news_title,news_desc,news_image_url,news_source_url))
     conn.commit()
     if cursor.rowcount > 0:
         return jsonify({"status":1, "message":"content saved"})
     else:
         return jsonify({"status" : 0,"message":"failed!"})
     
+# SAVED CONTENT PAGE
+@app.route('/saved_c')
+def saved_content_page():
+    cursor.execute("SELECT * FROM saved_content ORDER BY created_at DESC")
+    row = cursor.fetchall()
+    return render_template("saved.html", contents=row)
 
+    
 
 if __name__ ==  "__main__":
     app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -1,34 +1,107 @@
-const create_button = document.querySelector('#create_btn')
-create_button.addEventListener('click', saveContent)
+// const create_button = document.querySelector('#create_btn')
+// create_button.addEventListener('click', saveContent)
 
-async function saveContent() {
+// async function saveContent(button) {
 
-    let title = document.getElementById('news_title').innerText;
-    let description = document.getElementById('news_desc').innerText;
-    let image_url = document.getElementById('news_img').src;
-    let source_url = document.getElementById('news_source_url').href;
+//     let card = button.closest('.card');
+//     let title = card.document.querySelector('.news_title').innerText;
+//     let description = card.document.querySelector('.card-news-description').innerText;
+//     let image_url = card.document.querySelector('#news_img').src;
+//     let source_url = card.document.querySelector('#news_source_url').href;
+//     console.log(title)
+
+//     try {
+//         const response = await fetch('/savec', {
+//             method: "POST",
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({
+//                 'news_title': title,
+//                 'news_desc': description,
+//                 'news_image_url': image_url,
+//                 'news_source_url': source_url,
+//             })
+//         });
+
+//         const data = await response.json();
+//         if (data.status == 0) {
+//             console.log(`Status:${data.message}`);
+//         } else if (data.status == 1) {
+//             console.log(`Status:${data.message}`);
+//         }
+//     } catch (err) {
+//         console.log(err);
+//     }
+
+//     // await fetch('/savec', {
+//     //     method: "POST",
+//     //     headers: {
+//     //         'Content-Type': 'application/json'
+//     //     },
+//     //     body: JSON.stringify({
+//     //         'news_title': title,
+//     //         'news_desc': description,
+//     //         'news_image_url': image_url,
+//     //         'news_source_url': source_url,
+//     //     })
+//     // })
+//     //     .then(res => res.json())
+//     //     .then(response => {
+//     //         if (response.status == 0) {
+//     //             console.log(`Status:${response.message}`)
+//     //         }
+//     //         else if (response.status == 1) {
+//     //             console.log(`Status:${response.message}`)
+//     //         }
+//     //     })
+//     //     .catch(err => console.log(err))
+// }
+
+
+
+
+async function saveContent(button) {
+    let card = button.closest('.card'); // Get the parent card element
+    let titleElement = card.querySelector('.news_title');
+    let descriptionElement = card.querySelector('.card-news-description');
+    let imageElement = card.querySelector('#news_img');
+    let sourceElement = card.querySelector('#news_source_url');
+
+    if (!titleElement || !descriptionElement || !imageElement || !sourceElement) {
+        console.log("Error: Missing required elements in the card.");
+        return;
+    }
+
+    let title = titleElement.innerText;
+    let description = descriptionElement.innerText;
+    let image_url = imageElement.src;
+    let source_url = sourceElement.href;
     console.log(title)
 
-    await fetch('/savec', {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            'news_title': title,
-            'news_desc': description,
-            'news_image_url': image_url,
-            'news_source_url': source_url,
-        })
-    })
-        .then(res => res.json())
-        .then(response => {
-            if (response.status == 0) {
-                console.log(`Status:${response.message}`)
-            }
-            else if (response.status == 1) {
-                console.log(`Status:${response.message}`)
-            }
-        })
-        .catch(err => console.log(err))
+    try {
+        const response = await fetch('/savec', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'news_title': title,
+                'news_desc': description,
+                'news_image_url': image_url,
+                'news_source_url': source_url,
+            })
+        });
+
+        const data = await response.json();
+        if (data.status == 0) {
+            console.log(`Status:${data.message}`);
+        } else if (data.status == 1) {
+            console.log(`Status:${data.message}`);
+        }
+    } catch (err) {
+        console.log(err);
+    }
+
+
 }
